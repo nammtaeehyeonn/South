32117b4cb0655f4d797edaa832124dd42f6f870c
nammtaeehyeonn <skaxogusdl@gmail.com>
2024-02-17 15:41:54 +0900

    qwewqe

--- position.py
+++ position.py
@@ -1,31 +1,82 @@
-import streamlit as st
 import matplotlib.pyplot as plt
+from PIL import Image
+import numpy as np
+import streamlit as st
 
-# 운동장(큰 네모) 그리기
-def draw_field():
+# 이미지 파일 로드
+image_path = 'playground.png'  # 이미지 파일 경로 설정
+image = Image.open(image_path)
+width, height = image.size
+
+# 이미지 위에 그래픽 그리기
+def draw_on_image(image, *args):
     fig, ax = plt.subplots()
-    # 운동장의 경계를 그립니다. 여기서는 100x50 크기를 가정합니다.
-    field = plt.Rectangle((0,0), 100, 50, edgecolor="black", facecolor="none")
-    ax.add_patch(field)
+    ax.imshow(image)
     
-    # 포지션(동그라미) 추가 - 예시로 몇 개의 포지션을 임의로 추가합니다.
-    for x, y in [(20, 25), (40, 10), (40, 40), (60, 25)]:
-        player = plt.Circle((x, y), 2, color="blue")
-        ax.add_patch(player)
+    virtical_relative_positions = []
+    horizontal_relative_positions = []
     
-    ax.set_xlim(0, 100)
-    ax.set_ylim(0, 50)
-    ax.set_aspect('equal')  # 운동장의 가로세로 비율을 유지합니다.
+    virtical_rp = 0.75
+    for _ in range(len(args)):
+        virtical_relative_positions.append(round(virtical_rp,5))
+        virtical_rp -= 0.55/(len(args)-1)
+    # virtical_relative_positions += [0.9]
+    
+    for nums in args:
+        horizontal_rp = 0.2
+        horizontal_relative_per_positions = []
+        for _ in range(nums):
+            horizontal_relative_per_positions.append(round(horizontal_rp,5))
+            if nums == 1:
+                horizontal_rp += 0.6/(1)
+            else:
+                horizontal_rp += 0.6/(nums-1)
+        if len(horizontal_relative_per_positions) == 1:
+            horizontal_relative_per_positions = [0.5]
+        if len(horizontal_relative_per_positions) == 2:
+            horizontal_relative_per_positions = [0.4, 0.6]
+        if len(horizontal_relative_per_positions) == 3:
+            horizontal_relative_per_positions = [0.3, 0.5, 0.7]
+        horizontal_relative_positions.append(horizontal_relative_per_positions)
+    
+    # 포지션(동그라미) 그리기 - 예시로 몇 개의 포지션을 임의로 추가
+    for virtical_pos, horizontal_pos_list in zip(virtical_relative_positions, horizontal_relative_positions):
+        circle_y = virtical_pos * height  # 세로 위치 계산
+        for horizontal_pos in horizontal_pos_list:
+            circle = plt.Circle((width * horizontal_pos, circle_y), width * 0.05, color="red", fill=False)
+            ax.add_patch(circle)
+            
+    keep_circle_y = 0.9 * height        
+    keep_circle = plt.Circle((width * 0.5, keep_circle_y), width * 0.05, color='yellow', fill=False)
+    ax.add_patch(keep_circle)           
+    
+    # 축과 레이블 제거
+    ax.axis('off')
+    
+    print(virtical_relative_positions)
+    print(horizontal_relative_positions)
     
     return fig
 
-# Streamlit 앱에서 그래픽 표시
-def app():
-    st.title("축구 포지션 자동 배치")
+left_column, chat_column, right_column = st.columns([1, 2, 1])
+formation = '선택','3-5-2','3-4-3','3-3-3-1','3-4-1-2','3-6-1','3-4-2-1','4-4-2','4-3-3','4-2-3-1','4-3-1-2','4-2-2-2','4-3-2-1','4-1-4-1','4-1-2-3','4-5-1','4-4-1-1','4-6-0','5-3-2','5-4-1'
+with left_column:
+    Quater_1 = st.selectbox("1쿼터 포메이션 선택", formation)
+    st.write()
+    Quater_2 = st.selectbox("2쿼터 포메이션 선택", formation)
+    st.write()
+    Quater_3 = st.selectbox("3쿼터 포메이션 선택", formation)
+    st.write()
+    Quater_4 = st.selectbox("4쿼터 포메이션 선택", formation)
+    st.write()
+    
+    make_formation = st.button("실행하기")
+    if make_formation:
+        print(Quater_1)
+        print(Quater_2)
+        print(Quater_3)
+        print(Quater_4)
+        
+fig = draw_on_image(image,4,3,3)
 
-    st.write("운동장 및 포지션 시각화")
-    fig = draw_field()
-    st.pyplot(fig)
-
-if __name__ == "__main__":
-    app()
+st.pyplot(fig)
