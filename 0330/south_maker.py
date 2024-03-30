156a51f861bf243452239d4293e6545f9aee6fd9
namtaehyeon <skaxogusdl@gmail.com>
2024-03-30 12:39:18 +0900

    2024-03-30 12:39:17

--- 0330/south_maker.py
+++ 0330/south_maker.py
@@ -13,6 +13,24 @@ import time
 import datetime
 import subprocess
 
+from pymongo.mongo_client import MongoClient
+
+uri = "mongodb+srv://skaxogusdl:skaclxo661@southdb.h5j75si.mongodb.net/?retryWrites=true&w=majority&appName=SOUTHDB"
+
+# Create a new client and connect to the server
+client = MongoClient(uri)
+try:
+    client.admin.command('ping')
+    print("Pinged your deployment. You successfully connected to MongoDB!")
+except Exception as e:
+    print(e)
+    
+
+db = client.mydb
+
+    
+    
+    
 all_players_list = ["김동선" ,"김선광" ,"김성재" ,"김영목" ,"김은민" ,"김철영" ,"남태현" ,"민병인" ,"박창후" ,"서윤찬" ,"서종민" ,"소지호" ,"이병훈" ,"이산호" ,"이재성" ,"이종현" ,"정지원" ,"조성민" ,"조영수" ,"차민재" ,"차종수" ,"최민규" ,"최형근" ,"최형주" ,"구형준" ,"홍태호", "용병1", "용병2", "용병3", "용병4", "용병5" ]
 
 st.title("SOUTH_MAKER")
@@ -32,43 +50,52 @@ with st.expander('**2️⃣ 스쿼드 입력**'):
     players = st.multiselect("**참가 인원**", all_players_list)
 
 
-if st.button("정보 저장"):
-    with open("./123.txt", 'a') as f:
-        f.write(str(date))
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+###################################################################################################
 
 
     
-# # 버튼을 생성합니다.
-# if st.button('깃허브에 커밋 & 푸시하기'):
-#     # 현재 시간을 커밋 메시지로 사용합니다.
-#     commit_message = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-
-#     # subprocess를 사용하여 깃 명령어를 실행합니다.
-#     try:
-#         # 깃 add 명령어를 실행합니다.
-#         subprocess.run(["git", "add", "."], check=True, stderr=subprocess.PIPE)
-#         # 깃 commit 명령어를 실행합니다.
-#         subprocess.run(["git", "commit", "-m", commit_message], check=True, stderr=subprocess.PIPE)
-#         # 깃 push 명령어를 실행합니다.
-#         subprocess.run(["git", "push"], check=True, stderr=subprocess.PIPE)
-#         st.success('깃허브에 성공적으로 커밋 & 푸시되었습니다.')
-#     except subprocess.CalledProcessError as e:
-#         st.error(f'명령어 실행 중 오류가 발생했습니다: {e}')
-
-
-if st.button("깃허브에 커밋 & 푸시하기"):
+if st.button('깃허브에 커밋 & 푸시하기'):
+    # 현재 시간을 커밋 메시지로 사용합니다.
     commit_message = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-    
+
     try:
-        # 깃 명령어 실행
-        subprocess.run(["git", "config", "--global", "user.name", "namtaehyeon"], check=True)
-        subprocess.run(["git", "config", "--global", "user.email", "skaxogusdl@gmail.com"], check=True)
-        subprocess.run(["git", "add", "."], check=True)
-        subprocess.run(["git", "commit", "-m", commit_message], check=True)
-        subprocess.run(["git", "push"], check=True)
-        st.success("깃허브에 성공적으로 커밋 & 푸시되었습니다.")
+        subprocess.run(["git", "add", "."], check=True, stderr=subprocess.PIPE)
+        subprocess.run(["git", "commit", "-m", commit_message], check=True, stderr=subprocess.PIPE)
+        subprocess.run(["git", "push"], check=True, stderr=subprocess.PIPE)
+        st.success('깃허브에 성공적으로 커밋 & 푸시되었습니다.')
     except subprocess.CalledProcessError as e:
-        st.error(f"명령어 실행 중 오류가 발생했습니다: {e}")
+        st.error(f'명령어 실행 중 오류가 발생했습니다: {e}')
+
