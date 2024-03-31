e0841cea2021a59b80b290acdef11a192786bbb6
namtaehyeon <skaxogusdl@gmail.com>
2024-03-31 13:53:16 +0900

    2024-03-31 13:53:16

--- 0330/south_maker.py
+++ 0330/south_maker.py
@@ -93,42 +93,49 @@ with st.expander('**2️⃣ 스쿼드 입력**'):
     st.session_state['squad_info']['players'] = players
     entry_df = pd.DataFrame()
     if st.session_state['squad_info']['players']:
-        entry_df = pd.DataFrame([{"선수명":p, "쿼터 수":0, "주포지션":all_entry_dict[p]["주포지션"], "부포지션":'/'.join(all_entry_dict[p]["부포지션"])} for p in players], index = [idx+1 for idx in range(len(players))])
+        entry_df = pd.DataFrame([{"선수명":p, "쿼터 수":0, "주포지션":all_entry_dict[p]["주포지션"], "부포지션":','.join(all_entry_dict[p]["부포지션"])} for p in players], index = [idx+1 for idx in range(len(players))])
         edited_entry_df = st.data_editor(entry_df, use_container_width=True,
                                          column_config={
+                                            "주포지션": st.column_config.TextColumn(
+                                                validate='^(GK|CB|WB|CM|WM|CF|WF)$'
+                                            ), 
+                                            "부포지션": st.column_config.TextColumn(
+                                                validate='^(GK|CB|WB|CM|WM|CF|WF)(,(GK|CB|WB|CM|WM|CF|WF))*$'
+                                            ), 
                                             "쿼터 수": st.column_config.NumberColumn(
                                                 min_value=1,
                                                 max_value=4,
                                                 step=1,
                                             )},
                                          )
-        find_sub_pos_series = entry_df['주포지션'] + ","+ entry_df['부포지션'].apply(lambda x : ",".join(x))
+        find_sub_pos_series = edited_entry_df['주포지션'] + ","+ edited_entry_df['부포지션']
         st.session_state['squad_info']['players'] = json.loads(edited_entry_df.to_json(orient='records'))
         st.write("")
         st.write("**스쿼드 분석**")
         
         main_pos_list = []
         sub_pos_list = []
-        for i in ['GK','CB', 'WB', 'CM', 'WM', 'CF', 'WF']:
-            main_pos_list.append((entry_df['주포지션'] == i).sum()) 
+        for i in ['GK','CB', 'CM', 'CF', 'WB', 'WM', 'WF']:
+            main_pos_list.append((edited_entry_df['주포지션'] == i).sum()) 
             sub_pos_list.append((find_sub_pos_series.apply(lambda x : i in x)).sum())
+        
             
-        gk_count = (entry_df['주포지션'] == 'GK').sum()
+        gk_count = (edited_entry_df['주포지션'] == 'GK').sum()
         gk_quarter = 4 if gk_count == 0 else 4/gk_count
-        except_gk_count = len(entry_df) - (entry_df['주포지션'] == 'GK').sum()
+        except_gk_count = len(edited_entry_df) - (edited_entry_df['주포지션'] == 'GK').sum()
         except_gk_quarter = 44 if gk_count == 0 else 40
         
         if len(players) >= 11:
             if gk_count > 0:
                 col1, col2, col3, col4 = st.columns(4)
-                col1.metric("전체인원", f"총 {len(entry_df)}명", "")
+                col1.metric("전체인원", f"총 {len(edited_entry_df)}명", "")
                 col2.metric("골키퍼", f"{gk_count}명", f"{int(gk_quarter)}쿼터")
                 col3.metric(" ", f"{except_gk_count- int(except_gk_quarter%except_gk_count)}명", f"{int(except_gk_quarter/except_gk_count)}쿼터")
                 if int(except_gk_quarter/except_gk_count) != 4:
                     col4.metric(" ", f"{int(except_gk_quarter%except_gk_count)}명", f"{int(except_gk_quarter/except_gk_count)+1}쿼터")
             else:
                 col1, col2, col3 = st.columns(3)
-                col1.metric("전체인원", f"총 {len(entry_df)}명", f"골키퍼:{gk_count}명")
+                col1.metric("전체인원", f"총 {len(edited_entry_df)}명", f"골키퍼:{gk_count}명")
                 col2.metric(" ", f"{except_gk_count- int(except_gk_quarter%except_gk_count)}명", f"{int(except_gk_quarter/except_gk_count)}쿼터")
                 if int(except_gk_quarter/except_gk_count) != 4:
                     col3.metric(" ", f"{int(except_gk_quarter%except_gk_count)}명", f"{int(except_gk_quarter/except_gk_count)+1}쿼터")
@@ -173,6 +180,7 @@ if len(players) >= 11:
         st.session_state['formation_info']['formation'] = {"1q": formation1, "2q": formation2, "3q": formation3, "4q": formation4}
         
         if "선택" not in list(st.session_state['formation_info']['formation'].values()):
+            edited_entry_df_copy = pd.DataFrame(st.session_state['squad_info']['players'])
             formation_list = list(st.session_state['formation_info']['formation'].values())
             tab1, tab2, tab3, tab4 = st.tabs(["**▪1쿼터▪**", "**▪2쿼터▪**", "**▪3쿼터▪**", "**▪4쿼터▪**"])
             con_dict = {}
@@ -196,21 +204,21 @@ if len(players) >= 11:
                                 cols1, cols2, cols3, cols4 = st.columns(4)
                                 if cols_num == '2':
                                     if (splited_formation == ['4','2','2','2']) & ((horizon_cont_count)*(-1) == -2):
-                                        cols_num2_1 = cols1.selectbox('tmp', entry_df['선수명'], key=f"selected_key : tab{tdx+1}, container{horizon_cont_count}, cols_num{cols_num}, 1", label_visibility="collapsed",index=None,placeholder="L"+placeholder_list[0])
-                                        cols_num2_2 = cols4.selectbox('tmp', entry_df['선수명'], key=f"selected_key : tab{tdx+1}, container{horizon_cont_count}, cols_num{cols_num}, 2", label_visibility="collapsed",index=None,placeholder="R"+placeholder_list[1])
+                                        cols_num2_1 = cols1.selectbox('tmp', edited_entry_df_copy['선수명'], key=f"selected_key : tab{tdx+1}, container{horizon_cont_count}, cols_num{cols_num}, 1", label_visibility="collapsed",index=None,placeholder="L"+placeholder_list[0])
+                                        cols_num2_2 = cols4.selectbox('tmp', edited_entry_df_copy['선수명'], key=f"selected_key : tab{tdx+1}, container{horizon_cont_count}, cols_num{cols_num}, 2", label_visibility="collapsed",index=None,placeholder="R"+placeholder_list[1])
                                     else:
-                                        cols_num2_1 = cols2.selectbox('tmp', entry_df['선수명'], key=f"selected_key : tab{tdx+1}, container{horizon_cont_count}, cols_num{cols_num}, 3", label_visibility="collapsed",index=None,placeholder=placeholder_list[0])
-                                        cols_num2_2 = cols3.selectbox('tmp', entry_df['선수명'], key=f"selected_key : tab{tdx+1}, container{horizon_cont_count}, cols_num{cols_num}, 4", label_visibility="collapsed",index=None,placeholder=placeholder_list[1])
+                                        cols_num2_1 = cols2.selectbox('tmp', edited_entry_df_copy['선수명'], key=f"selected_key : tab{tdx+1}, container{horizon_cont_count}, cols_num{cols_num}, 3", label_visibility="collapsed",index=None,placeholder=placeholder_list[0])
+                                        cols_num2_2 = cols3.selectbox('tmp', edited_entry_df_copy['선수명'], key=f"selected_key : tab{tdx+1}, container{horizon_cont_count}, cols_num{cols_num}, 4", label_visibility="collapsed",index=None,placeholder=placeholder_list[1])
                                     if cols_num2_1:
                                         st.session_state['formation_info'][f'{tdx+1}q'][horizon_cont_count-1] = [cols_num2_1,
                                                                                                                 st.session_state['formation_info'][f'{tdx+1}q'][horizon_cont_count-1][1]]
                                     if cols_num2_2:
                                         st.session_state['formation_info'][f'{tdx+1}q'][horizon_cont_count-1] = [st.session_state['formation_info'][f'{tdx+1}q'][horizon_cont_count-1][0], cols_num2_2]
                                 else:
-                                    cols_num4_1 = cols1.selectbox('tmp',entry_df['선수명'], key=f"selected_key : tab{tdx+1}, container{horizon_cont_count}, cols_num{cols_num}, 1", label_visibility="collapsed",index=None,placeholder="L"+placeholder_list[0])
-                                    cols_num4_2 = cols2.selectbox('tmp',entry_df['선수명'], key=f"selected_key : tab{tdx+1}, container{horizon_cont_count}, cols_num{cols_num}, 2", label_visibility="collapsed",index=None,placeholder=placeholder_list[1])
-                                    cols_num4_3 = cols3.selectbox('tmp',entry_df['선수명'], key=f"selected_key : tab{tdx+1}, container{horizon_cont_count}, cols_num{cols_num}, 3", label_visibility="collapsed",index=None,placeholder=placeholder_list[2])
-                                    cols_num4_4 = cols4.selectbox('tmp',entry_df['선수명'], key=f"selected_key : tab{tdx+1}, container{horizon_cont_count}, cols_num{cols_num}, 4", label_visibility="collapsed",index=None,placeholder="R"+placeholder_list[3])
+                                    cols_num4_1 = cols1.selectbox('tmp',edited_entry_df_copy['선수명'], key=f"selected_key : tab{tdx+1}, container{horizon_cont_count}, cols_num{cols_num}, 1", label_visibility="collapsed",index=None,placeholder="L"+placeholder_list[0])
+                                    cols_num4_2 = cols2.selectbox('tmp',edited_entry_df_copy['선수명'], key=f"selected_key : tab{tdx+1}, container{horizon_cont_count}, cols_num{cols_num}, 2", label_visibility="collapsed",index=None,placeholder=placeholder_list[1])
+                                    cols_num4_3 = cols3.selectbox('tmp',edited_entry_df_copy['선수명'], key=f"selected_key : tab{tdx+1}, container{horizon_cont_count}, cols_num{cols_num}, 3", label_visibility="collapsed",index=None,placeholder=placeholder_list[2])
+                                    cols_num4_4 = cols4.selectbox('tmp',edited_entry_df_copy['선수명'], key=f"selected_key : tab{tdx+1}, container{horizon_cont_count}, cols_num{cols_num}, 4", label_visibility="collapsed",index=None,placeholder="R"+placeholder_list[3])
                                     if cols_num4_1:
                                         st.session_state['formation_info'][f'{tdx+1}q'][horizon_cont_count-1] = [cols_num4_1,
                                                                                                                 st.session_state['formation_info'][f'{tdx+1}q'][horizon_cont_count-1][1],
@@ -235,14 +243,14 @@ if len(players) >= 11:
                             if cols_num in ['1','3','5']:
                                 cols1, cols2, cols3, cols4, cols5 = st.columns(5)
                                 if cols_num == '1':
-                                    cols_num1_1 = cols3.selectbox('tmp',entry_df['선수명'], key=f"selected_key : tab{tdx+1}, container{horizon_cont_count}, cols_num{cols_num}, 3", label_visibility="collapsed",index=None,placeholder=placeholder_list[0])
+                                    cols_num1_1 = cols3.selectbox('tmp',edited_entry_df_copy['선수명'], key=f"selected_key : tab{tdx+1}, container{horizon_cont_count}, cols_num{cols_num}, 3", label_visibility="collapsed",index=None,placeholder=placeholder_list[0])
                                     if cols_num1_1:
                                         st.session_state['formation_info'][f'{tdx+1}q'][horizon_cont_count-1] = [cols_num1_1]
                                     # for_session_list = cols_num1_1
                                 if cols_num == '3':
-                                    cols_num3_1 = cols2.selectbox('tmp',entry_df['선수명'], key=f"selected_key : tab{tdx+1}, container{horizon_cont_count}, cols_num{cols_num}, 2", label_visibility="collapsed",index=None,placeholder="L"+placeholder_list[0])
-                                    cols_num3_2 = cols3.selectbox('tmp',entry_df['선수명'], key=f"selected_key : tab{tdx+1}, container{horizon_cont_count}, cols_num{cols_num}, 3", label_visibility="collapsed",index=None,placeholder=placeholder_list[1])
-                                    cols_num3_3 = cols4.selectbox('tmp',entry_df['선수명'], key=f"selected_key : tab{tdx+1}, container{horizon_cont_count}, cols_num{cols_num}, 4", label_visibility="collapsed",index=None,placeholder="R"+placeholder_list[2])
+                                    cols_num3_1 = cols2.selectbox('tmp',edited_entry_df_copy['선수명'], key=f"selected_key : tab{tdx+1}, container{horizon_cont_count}, cols_num{cols_num}, 2", label_visibility="collapsed",index=None,placeholder="L"+placeholder_list[0])
+                                    cols_num3_2 = cols3.selectbox('tmp',edited_entry_df_copy['선수명'], key=f"selected_key : tab{tdx+1}, container{horizon_cont_count}, cols_num{cols_num}, 3", label_visibility="collapsed",index=None,placeholder=placeholder_list[1])
+                                    cols_num3_3 = cols4.selectbox('tmp',edited_entry_df_copy['선수명'], key=f"selected_key : tab{tdx+1}, container{horizon_cont_count}, cols_num{cols_num}, 4", label_visibility="collapsed",index=None,placeholder="R"+placeholder_list[2])
                                     if cols_num3_1:
                                         st.session_state['formation_info'][f'{tdx+1}q'][horizon_cont_count-1] = [cols_num3_1,
                                                                                                                 st.session_state['formation_info'][f'{tdx+1}q'][horizon_cont_count-1][1], 
@@ -257,11 +265,11 @@ if len(players) >= 11:
                                                                                                                 cols_num3_3]
                                     # for_session_list = cols_num3_1 + cols_num3_2 + cols_num3_3
                                 if cols_num == '5':
-                                    cols_num5_1 = cols1.selectbox('tmp',entry_df['선수명'], key=f"selected_key : tab{tdx+1}, container{horizon_cont_count}, cols_num{cols_num}, 1", label_visibility="collapsed",index=None,placeholder="L"+placeholder_list[0])
-                                    cols_num5_2 = cols2.selectbox('tmp',entry_df['선수명'], key=f"selected_key : tab{tdx+1}, container{horizon_cont_count}, cols_num{cols_num}, 2", label_visibility="collapsed",index=None,placeholder=placeholder_list[1])
-                                    cols_num5_3 = cols3.selectbox('tmp',entry_df['선수명'], key=f"selected_key : tab{tdx+1}, container{horizon_cont_count}, cols_num{cols_num}, 3", label_visibility="collapsed",index=None,placeholder=placeholder_list[2])
-                                    cols_num5_4 = cols4.selectbox('tmp',entry_df['선수명'], key=f"selected_key : tab{tdx+1}, container{horizon_cont_count}, cols_num{cols_num}, 4", label_visibility="collapsed",index=None,placeholder=placeholder_list[3])
-                                    cols_num5_5 = cols5.selectbox('tmp',entry_df['선수명'], key=f"selected_key : tab{tdx+1}, container{horizon_cont_count}, cols_num{cols_num}, 5", label_visibility="collapsed",index=None,placeholder="R"+placeholder_list[4])
+                                    cols_num5_1 = cols1.selectbox('tmp',edited_entry_df_copy['선수명'], key=f"selected_key : tab{tdx+1}, container{horizon_cont_count}, cols_num{cols_num}, 1", label_visibility="collapsed",index=None,placeholder="L"+placeholder_list[0])
+                                    cols_num5_2 = cols2.selectbox('tmp',edited_entry_df_copy['선수명'], key=f"selected_key : tab{tdx+1}, container{horizon_cont_count}, cols_num{cols_num}, 2", label_visibility="collapsed",index=None,placeholder=placeholder_list[1])
+                                    cols_num5_3 = cols3.selectbox('tmp',edited_entry_df_copy['선수명'], key=f"selected_key : tab{tdx+1}, container{horizon_cont_count}, cols_num{cols_num}, 3", label_visibility="collapsed",index=None,placeholder=placeholder_list[2])
+                                    cols_num5_4 = cols4.selectbox('tmp',edited_entry_df_copy['선수명'], key=f"selected_key : tab{tdx+1}, container{horizon_cont_count}, cols_num{cols_num}, 4", label_visibility="collapsed",index=None,placeholder=placeholder_list[3])
+                                    cols_num5_5 = cols5.selectbox('tmp',edited_entry_df_copy['선수명'], key=f"selected_key : tab{tdx+1}, container{horizon_cont_count}, cols_num{cols_num}, 5", label_visibility="collapsed",index=None,placeholder="R"+placeholder_list[4])
                                     if cols_num5_1:
                                         st.session_state['formation_info'][f'{tdx+1}q'][horizon_cont_count-1] = [cols_num5_1,
                                                                                                                 st.session_state['formation_info'][f'{tdx+1}q'][horizon_cont_count-1][1],
@@ -298,7 +306,7 @@ if len(players) >= 11:
                         for_session_list_GK = []
                         st.markdown('<span style="color:blue; font-weight:bold; font-size:25px;">GK</span>', unsafe_allow_html=True)
                         cols1, cols2, cols3, cols4, cols5 = st.columns(5)
-                        for_session_list_GK = cols3.selectbox('tmp',entry_df['선수명'], key=f"selected_key : GK_{tdx}", label_visibility="collapsed",index=None,placeholder="GK")
+                        for_session_list_GK = cols3.selectbox('tmp',edited_entry_df_copy['선수명'], key=f"selected_key : GK_{tdx}", label_visibility="collapsed",index=None,placeholder="GK")
                         if for_session_list_GK:
                             st.session_state['formation_info'][f'{tdx+1}q'][-1] = [for_session_list_GK]
 
